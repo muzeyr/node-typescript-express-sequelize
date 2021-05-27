ccbe6a0ae16c4fc3ddb56961742969d46b690ed6
Uzeyr OZCAN <muzeyr@gmail.com>
2021-05-27 21:52:41 +0300

    validation error

--- build/endpoints/customers/customers.post.js
+++ build/endpoints/customers/customers.post.js
@@ -3,17 +3,9 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const _index_1 = require("../../dao/_index");
 function create(req, res) {
     console.log(2222);
-    req.getValidationResult(req)
-        .then(function (result) {
-        if (result.isEmpty()) {
-            return _index_1.AppCustomerDao.create(req.body)
-                .then(appuser => res.status(201).send(appuser))
-                .catch(error => res.boom.badRequest(error));
-        }
-        else {
-            res.boom.badRequest('Validation errors', result.mapped());
-        }
-    });
+    _index_1.AppCustomerDao.create(req.body)
+        .then(appuser => res.status(201).send(appuser))
+        .catch(error => res.boom.badRequest(error));
 }
 exports.create = create;
 
--- src/endpoints/customers/customers.post.ts
+++ src/endpoints/customers/customers.post.ts
@@ -4,15 +4,9 @@ import { AppCustomerDao } from '../../dao/_index'
 
 export function create(req, res: Response) {
     console.log(2222);
+    AppCustomerDao.create(req.body)
+        .then(appuser => res.status(201).send(appuser))
+        .catch(error => res.boom.badRequest(error));
 
-    req.getValidationResult(req)
-        .then(function(result) {
-            if (result.isEmpty()) {
-                return AppCustomerDao.create(req.body)
-                    .then(appuser => res.status(201).send(appuser))
-                    .catch(error => res.boom.badRequest(error))
-            } else {
-                res.boom.badRequest('Validation errors', result.mapped())
-            }
-        })
+
 }
